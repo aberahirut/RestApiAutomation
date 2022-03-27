9fee1af83307c9ee4e094451312b2f0407a603ee
Hirut Abera <aberahirut48@gmail.com>
2022-03-27 11:56:16 -0400

    Test Updated

--- RestAPIAutomation/src/test/java/com/testng/project/RestAPIAutomation/GetAPITest.java
+++ RestAPIAutomation/src/test/java/com/testng/project/RestAPIAutomation/GetAPITest.java
@@ -13,21 +13,31 @@ import com.google.gson.Gson;
 public class GetAPITest {
 
 	public static void main(String[] args) {
-	int pageNo = 2;
-	RestAssured.baseURI="https://reqres.in/";
-	String responseStr = given().log().all().when().get("/api/users?page="+pageNo)
-	.then().assertThat().statusCode(200).extract().response().asString();
-	System.out.println(responseStr);
+	String firstPage = getInputResponse(1);
+	String secondPage = getInputResponse(2);
 	
-	//JSONObject json = new JSONObject(responseStr);
-	/*JsonPath parser = new JsonPath(responseStr);  
-	System.out.println(parser.toString());*/
+
 	 Gson g = new Gson();
-	 Root root = g.fromJson(responseStr,Root.class);
+	 Root firstPageRoot = g.fromJson(firstPage,Root.class);
+	 assertPageDetails(firstPageRoot,1);
+	 Root secondPageRootObj =g.fromJson(secondPage,Root.class); 
+	 assertPageDetails(secondPageRootObj,1);
 	 
-	Assert.assertEquals(root.getPage(),pageNo);
-	Assert.assertEquals(root.getData().size(),root.per_page);
+	 int totalDataCount = firstPageRoot.getData().size()+secondPageRootObj.getData().size();
+	 Assert.assertEquals(totalDataCount,firstPageRoot.getTotal());
+	 Assert.assertEquals(totalDataCount,secondPageRootObj.getTotal());
+
 	 
 	}
 	
+	public static String getInputResponse(int pageNumber) {
+		RestAssured.baseURI="https://reqres.in/";
+		return given().log().all().when().get("/api/users?page="+pageNumber)
+		.then().assertThat().statusCode(200).extract().response().asString();
+	}
+	
+	public static void assertPageDetails(Root root,int pageNumber) {
+		Assert.assertEquals(root.getPage(),pageNumber);
+		Assert.assertEquals(root.getData().size(),root.per_page);
+	}
 }
